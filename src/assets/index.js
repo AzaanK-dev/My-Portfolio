7b72b200967e3f7b74bdb54b7cef6b58950b7fed
AzaanK-dev <azaanmehtabk@gmail.com>
2025-08-21 15:37:05 +0500

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,32 +7,21 @@ import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
-import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
-import git from "./tech/git.png";
 import html from "./tech/html.png";
+import css from "./tech/css.png";
 import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
+import figma from "./tech/figma.png";
 import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
-
+import git from "./tech/git.png";
 import java from "./tech/java.png";
 import cProg from "./tech/cProg.png";
 import firebase from "./tech/firebase.png";
 import msoffice from "./tech/msoffice.png";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
 import ccfj from "./company/ccfj.png";
 import techzone from "./company/techzone.png";
 import ubit from "./company/ubit.png";
-import tesla from "./company/tesla.png";
 
 import blogzaan from "./blogzaan.png";
 import bakeryweb from "./bakeryweb.png";
@@ -48,31 +37,20 @@ export {
   menu,
   close,
   css,
-  docker,
   figma,
   git,
   html,
   javascript,
-  mongodb,
-  nodejs,
   reactjs,
-  redux,
   tailwind,
-  typescript,
-  threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  java,
+  cProg,
+  firebase,
+  msoffice,
   blogzaan,
   bakeryweb,
   todoweb,
   ccfj,
   techzone,
   ubit,
-
-  java,
-  cProg,
-  firebase,
-  msoffice,
 };
